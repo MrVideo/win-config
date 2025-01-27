ca106078527a7d80d2eed7c2febdffc7a69e9729
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2025-01-27 15:47:23 +0100

    Add shortcuts for new tabs and splits

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,6 +17,21 @@ function gvk {
 # Use CTRL+D to exit
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
+# Use CTRL+T to open a new tab with the same working directory
+Set-PSReadlineKeyHandler -Key ctrl+t -ScriptBlock {
+	Start-Process wt -ArgumentList "-w", "0", "nt", "-d", "."
+}
+
+# Use CTRL+V to open a vertical split with the same working directory
+Set-PSReadlineKeyHandler -Key ctrl+v -ScriptBlock {
+	Start-Process wt -ArgumentList "-w", "0", "split-pane", "--vertical", "-d", "."
+}
+
+# Use CTRL+S to open a horizontal split with the same working directory
+Set-PSReadlineKeyHandler -Key ctrl+s -ScriptBlock {
+	Start-Process wt -ArgumentList "-w", "0", "split-pane", "--horizontal", "-d", "."
+}
+
 # List hidden files
 function la {
 	Get-ChildItem . -Force
